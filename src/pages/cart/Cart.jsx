c520333027b74accf96f11b06d592305ad000bf2
mriana9 <mariana.algafy@sharkod>
2025-06-09 18:49:58 +0300

    add: inc qty, dec qty, delete item, clear all

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Carts from "../../components/carts/Carts";
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import axios from "axios";
 import { toast, Slide } from "react-toastify";
 
